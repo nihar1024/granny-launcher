b3facf6e773b70131fb99d3719d9cc4ea59a131f
Nihar Thakkar <nihartar@gmail.com>
2018-01-17 12:12:35 +0530

    Removed Home Screen title and added All Apps button.

--- app/src/main/java/com/grannylauncher/HomeActivity.java
+++ app/src/main/java/com/grannylauncher/HomeActivity.java
@@ -1,9 +1,14 @@
 package com.grannylauncher;
 
+import android.app.Activity;
+import android.graphics.Color;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
 
-public class HomeActivity extends AppCompatActivity
+public class HomeActivity extends Activity
 {
 
     @Override
@@ -11,5 +16,11 @@ public class HomeActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+
+    @Override
+    public void onBackPressed()
+    {
+        // Do nothing
     }
 }
